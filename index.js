c49c0136a3ea1bd297c59fd73693d3533d5f09ce
Lewis <lewis.kagia@student.moringaschool.com>
2023-06-16 11:27:50 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solutions in this file
+const Sinon = 'Sinon'
+let spy;
+console.log(Sinon.spy)
+
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+
+  function countDown()  {
+    let spy;
+    console.log(chai.spy);
+    spy.restore();
+    it('invokes console.log once for each number, counting down from the number provided to zero', () => {
+        countDown(10);
+        expect(spy.callCount, "Expected countDown(10) to invoke 11 console.logs")
+          .to.eq(11);
+      });
+    
+      it('logs each number when counting down, starting from the number provided', () => {
+        countDown(4);
+        expect(spy.calledWithExactly(4), "Expected countDown(4) to log 4 first")
+          .to.be.true;
+        expect(spy.calledWithExactly(3), "Expected countDown(4) to log 3 after 4")
+          .to.be.true;
+        expect(spy.calledWithExactly(2), "Expected countDown(4) to log 2 after 3")
+          .to.be.true;
+        expect(spy.calledWithExactly(1), "Expected countDown(4) to log 1 after 2")
+          .to.be.true;
+        expect(spy.calledWithExactly(0), "Expected countDown(4) to log 0 after 1")
+          .to.be.true;
+      });
+    };
+
+  
